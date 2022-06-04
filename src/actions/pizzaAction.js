a77e920e3a1b440bb149b36b1cc8286c6ebb56c8
Sabarish Chandrasekaran <sabarish27k6@gmail.com>
2022-06-04 14:28:59 +0530

    changes

--- src/actions/pizzaAction.js
+++ src/actions/pizzaAction.js
@@ -3,7 +3,9 @@ import axios from "axios";
 export const getAllPizzas = () => async (dispatch) => {
   dispatch({ type: "GET_PIZZAS_REQUEST" });
   try {
-    const response = await axios.get("/api/pizzas/getAllPizzas");
+    const response = await axios.get(
+      "https://pizza-shop-app-mern.herokuapp.com/api/pizzas/getAllPizzas"
+    );
     console.log(response);
     dispatch({ type: "GET_PIZZAS_SUCCESS", payload: response.data });
   } catch (error) {
